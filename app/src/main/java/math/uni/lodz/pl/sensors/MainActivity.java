e2d75e58c59b353abb54f51ae6220c3b742ea8c3
Wiklos <wiklos24@interia.eu>
2018-01-03 23:02:38 +0100

    Flashlight
    Flashlight turn on when light sensor sense a dark environment

--- app/src/main/java/math/uni/lodz/pl/sensors/MainActivity.java
+++ app/src/main/java/math/uni/lodz/pl/sensors/MainActivity.java
@@ -9,11 +9,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     TextView textView;
     SensorManager manager;
     Sensor sensor;
+
+    //flashlight
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ImageButton mTorchOnOffButton;
+    private Boolean isTorchOn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +46,79 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         manager = (SensorManager)getSystemService(Service.SENSOR_SERVICE);
         sensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        //flashlight
+        isTorchOn = false;
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         manager.unregisterListener(this);
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         manager.registerListener(this, sensor,manager.SENSOR_DELAY_NORMAL);
+        if(isTorchOn){
+            turnOnFlashLight();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if(sensorEvent.sensor.getType()== Sensor.TYPE_LIGHT)
             textView.setText("" + sensorEvent.values[0]);
+        //flashlight
+        if(sensorEvent.values[0]<10)
+        {
+            //Toast.makeText(getApplicationContext(),"Toast",Toast.LENGTH_SHORT).show();
+            turnOnFlashLight();
+            isTorchOn = true;
+        }
+        else
+        {
+            turnOffFlashLight();
+            isTorchOn = false;
+        }
 
     }
 
@@ -49,5 +126,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
-    //////
+
+    //flashlight
+
+    public void turnOnFlashLight() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
