a58914c988c9874c4780deb594c54b9695eb59f4
Wiklos <wiklos24@interia.eu>
2018-01-06 23:24:26 +0100

    Flashlight on map actiity
    Displaying amount of lumens above the map, flashlight turning on when lumens<10

--- app/src/main/java/math/uni/lodz/pl/sensors/MapsActivity.java
+++ app/src/main/java/math/uni/lodz/pl/sensors/MapsActivity.java
@@ -1,14 +1,28 @@
 package math.uni.lodz.pl.sensors;
 
 import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.location.Location;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -28,13 +42,24 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+        LocationListener,SensorEventListener {
 
     private GoogleMap mMap;
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     Marker mCurrLocationMarker;
     LocationRequest mLocationRequest;
+
+    //flashlight
+    TextView textView;
+    SensorManager manager;
+    Sensor sensor;
+
+    //flashlight
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ImageButton mTorchOnOffButton;
+    private Boolean isTorchOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +73,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //flashlight
+        textView = (TextView)findViewById(R.id.textView3);
+
+        manager = (SensorManager)getSystemService(Service.SENSOR_SERVICE);
+        sensor = manager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        //flashlight
+        isTorchOn = false;
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(MapsActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -232,4 +292,86 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // You can add here other case statements according to your requirement.
         }
     }
+    //flashlight main
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.unregisterListener(this);
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        manager.registerListener(this, sensor,manager.SENSOR_DELAY_NORMAL);
+        if(isTorchOn){
+            turnOnFlashLight();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(isTorchOn){
+            turnOffFlashLight();
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor.getType()== Sensor.TYPE_LIGHT)
+            textView.setText("Lumens:" + sensorEvent.values[0]);
+        //flashlight
+        if(sensorEvent.values[0]<10)
+        {
+            //Toast.makeText(getApplicationContext(),"Toast",Toast.LENGTH_SHORT).show();
+            turnOnFlashLight();
+            isTorchOn = true;
+        }
+        else
+        {
+            turnOffFlashLight();
+            isTorchOn = false;
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    //flashlight
+
+    public void turnOnFlashLight() {
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void gotoMaps(View view)
+    {
+        startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+    }
+
 }
+
+
